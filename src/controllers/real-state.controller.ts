63a59771e5276980373d01cd3aad7bae3b923fb9
Vinicius Menezes <vinicius.menezes@meta.com.br>
2021-07-30 00:30:01 -0300

    joi validation

--- src/controllers/real-state.controller.ts
+++ src/controllers/real-state.controller.ts
@@ -1,10 +1,23 @@
 import { RealStateService } from '../services';
 import { Router } from 'express';
+import Joi from 'joi';
+import { Validate } from '../middlewares/validation';
 
 const router = Router();
 const realStateService = new RealStateService();
 
-router.get('/zap', realStateService.getZap);
-router.get('/vivareal', realStateService.getVivaReal);
+router.get('/zap',
+  Validate('query', {
+    limit: Joi.number().greater(0).default(10).optional(),
+    page: Joi.number().greater(0).default(1).optional()
+  }),
+  realStateService.getZap);
+
+router.get('/vivareal',
+  Validate('query', {
+    limit: Joi.number().greater(0).default(10).optional(),
+    page: Joi.number().greater(0).default(1).optional()
+  }),
+  realStateService.getVivaReal);
 
 export const RealStateController: Router = router;
